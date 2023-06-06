3b486c7ccaf5e59242705069abbdfa6da5eafdfa
nupior <135617467+nupior@users.noreply.github.com>
2023-06-07 00:27:40 +0300

    Add files via upload

--- lab03/model.py
+++ lab03/model.py
@@ -10,7 +10,7 @@
     <h1>Error 503 Backend is unhealthy</h1>
     <p>Backend is unhealthy</p>
     <h3>Error 54113</h3>
-    <p>Details: cache-ams12758-AMS 1686086815 3684725305</p>
+    <p>Details: cache-ams12773-AMS 1686086819 962875274</p>
     <hr>
     <p>Varnish cache server</p>
   </body>
